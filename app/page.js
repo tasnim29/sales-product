76674cd2f77a0ce483c0de3d527e8813478d1ac5
tasnim29 <tasnimmahmudnjr@gmail.com>
2025-12-01 19:51:37 +0600

    filter component is done scratchly

--- app/page.js
+++ app/page.js
@@ -1,6 +1,20 @@
 "use client";
 
+import { getSalesData, getToken } from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+
 const DashBoard = () => {
+  const [filters, setFilters] = useState({});
+  const { data: tokenData, isLoading } = useQuery(["token"], getToken);
+  const token = tokenData?.token;
+
+  // getSalesData
+  const { data: salesData, isLoading: salesLoading } = useQuery(
+    ["sales", token, filters],
+    () => getSalesData(token, filters),
+    { enabled: !!token, keepPreviousData: true }
+  );
   return (
     <div>
       <h1>Sales Dashboard</h1>
